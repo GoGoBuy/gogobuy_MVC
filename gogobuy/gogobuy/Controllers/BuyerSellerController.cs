70f5c98d57df60b8f9a7bdfb8d333c6429feabb6
Mars62689 <mars62689@gmail.com>
2021-07-12 03:49:08 +0800

    feat: 新增查看全部商品、願望商品頁面及功能 調整項目: 1._Layout.cshtml連結改致瀏覽全商品頁面 2.願望商品、商品管理清單頁面完成 3.新增瀏覽全願望、商品選擇分類功能

--- gogobuy/gogobuy/Controllers/BuyerSellerController.cs
+++ gogobuy/gogobuy/Controllers/BuyerSellerController.cs
@@ -1,10 +1,15 @@
 ﻿using gogobuy.Models;
+using gogobuy.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace gogobuy.Controllers
 {
     public class BuyerSellerController : Controller
     {
+        gogobuydbEntities db = new gogobuydbEntities();
+
         // GET: BuyerSeller
         public ActionResult BuyerUpload()
         {
@@ -20,7 +25,17 @@ namespace gogobuy.Controllers
             {
                 return RedirectToAction("Login", "Home");
             }
-            return View();
+            int userId = (int)Session[CDictionary.SK_LOGINED_USER_ID];
+            var wishProd = from w in db.tProduct
+                           where w.fIsWish == true && w.fMemberID == userId
+                           select w;
+            List<ProductViewModel> list = new List<ProductViewModel>();
+            foreach(var wishProds in wishProd)
+            {
+                list.Add(new ProductViewModel { fProductID = wishProds.fProductID, fProductName = wishProds.fProductName,
+                fImgPath = wishProds.fImgPath, fPrice = wishProds.fPrice, fQuantity = wishProds.fQuantity, fUpdateTime = wishProds.fUpdateTime});
+            }
+            return View(list);
         }
         public ActionResult SellerManagement()
         {
@@ -28,7 +43,24 @@ namespace gogobuy.Controllers
             {
                 return RedirectToAction("Login", "Home");
             }
-            return View();
+            int userId = (int)Session[CDictionary.SK_LOGINED_USER_ID];
+            var wishProd = from w in db.tProduct
+                           where w.fIsWish == false && w.fMemberID == userId
+                           select w;
+            List<ProductViewModel> list = new List<ProductViewModel>();
+            foreach (var wishProds in wishProd)
+            {
+                list.Add(new ProductViewModel
+                {
+                    fProductID = wishProds.fProductID,
+                    fProductName = wishProds.fProductName,
+                    fImgPath = wishProds.fImgPath,
+                    fPrice = wishProds.fPrice,
+                    fQuantity = wishProds.fQuantity,
+                    fUpdateTime = wishProds.fUpdateTime
+                });
+            }
+            return View(list);
         }
         public ActionResult BucketListDetails()
         {
@@ -37,6 +69,61 @@ namespace gogobuy.Controllers
         public ActionResult SellListDetails()
         {
             return View();
+        }
+        public ActionResult ProductList(string category)
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>();
+            var db = new gogobuydbEntities();
+            IEnumerable<tProduct> table;
+            if (string.IsNullOrEmpty(category))
+            {
+                table = db.tProduct.Where(p => p.fIsWish == false)
+                .Select(p => p);
+            } else
+            {
+                table = db.tProduct.Where(p => p.fIsWish == false && p.fCategory == category)
+                .Select(p => p);
+            }
+            foreach (var p in table)
+            {
+                products.Add(new ProductViewModel
+                {
+                    fProductID = p.fProductID,
+                    fProductName = p.fProductName,
+                    fImgPath = p.fImgPath,
+                    fPrice = p.fPrice,
+                    fProductLocation = p.fProductLocation
+                });
+            }
+            return View(products);
+        }
+        public ActionResult WishProductList(string category)
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>();
+            var db = new gogobuydbEntities();
+            IEnumerable<tProduct> table;
+            if (string.IsNullOrEmpty(category))
+            {
+                table = db.tProduct.Where(p => p.fIsWish == true)
+                .Select(p => p);
+            }
+            else
+            {
+                table = db.tProduct.Where(p => p.fIsWish == true && p.fCategory == category)
+                .Select(p => p);
+            }
+            foreach (var p in table)
+            {
+                products.Add(new ProductViewModel
+                {
+                    fProductID = p.fProductID,
+                    fProductName = p.fProductName,
+                    fImgPath = p.fImgPath,
+                    fPrice = p.fPrice,
+                    fProductLocation = p.fProductLocation
+                });
+            }
+            return View(products);
         }
     }
 }
--- gogobuy/gogobuy/ViewModels/ProductViewModel.cs
+++ gogobuy/gogobuy/ViewModels/ProductViewModel.cs
@@ -7,15 +7,14 @@ namespace gogobuy.ViewModels
 {
     public class ProductViewModel
     {
-      public ProductViewModel()
-        {
-            this.product = new tProduct();
-        }
-        public tProduct product { get; set; }
+        
         public int fProductID { get; set; }
         public string fProductName { get; set; }
         public string fImgPath { get; set; }
         public decimal? fPrice { get; set; }
         public string fProductLocation { get; set; }
+        public int? fQuantity { get; set; }
+        public string fArrivalTime { get; set; }
+        public DateTime? fUpdateTime { get; set; }
     }
 }
