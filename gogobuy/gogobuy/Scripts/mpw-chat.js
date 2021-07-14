83dfcc50ad10174cfc2d8336f022de9e8fcc7a15
Mars62689 <mars62689@gmail.com>
2021-07-14 13:51:43 +0800

    版本衝突暫存

--- gogobuy/gogobuy/Scripts/mpw-chat.js
+++ gogobuy/gogobuy/Scripts/mpw-chat.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function (mpwchat) {
 
     mpwchat.log = mpwchat.log || {};    //登入帳號
-    
+
     mpwchat.log.write = function (message) {
         //顯示登入訊息
         if (typeof (message) != typeof ("")) message = "[kb] Invalid Log Message Format!";
@@ -22,31 +22,31 @@
         //聊天室上線
         if (!joinedChat) {
             document.getElementById("txaChat").value = ""; //Reset chat box
-            mpwchat.log.write("[MPW Chat] Connecting...");
+            mpwchat.log.write("[GOGOBUY Chat] 連線中...");
             conn.stop();
             //Start connection
             conn.start().done(function () {
                 try {
-                    mpwchat.log.write("[MPW Chat] Connected to Chat Server");
+                    mpwchat.log.write("[GOGOBUY Chat] 連線到伺服器");
                     //請求上線
                     var userName = document.getElementById("txbUserName").value;
                     if (!userName) {
-                        mpwchat.log.write("[MPW Chat]{Error} Invalid Username");
+                        mpwchat.log.write("[GOGOBUY Chat]{Error} 錯誤的使用者名稱");
                     } else {
-                        mpwchat.log.write("[MPW Chat] Joining as " + userName);
+                        mpwchat.log.write("[GOGOBUY Chat] 歡迎 " + userName + " 加入");
                         gHub.invoke('userInformation', userName)
                             .done(function () {
                                 //請求送出
                             })
                             .fail(function (error) {
                                 //請求失敗
-                                mpwchat.log.write("[MPW Chat]{Error} " + error.message);
+                                mpwchat.log.write("[GOGOBUY Chat]{Error} " + error.message);
                                 conn.stop();
                                 joinedChat = false;
                             });
                     }
                 } catch (ex) {
-                    mpwchat.log.write("[MPW Chat]{Error} " + ex.message);
+                    mpwchat.log.write("[GOGOBUY Chat]{Error} " + ex.message);
                     conn.stop();
                     joinedChat = false;
                 }
@@ -70,7 +70,7 @@
                 })
                 .fail(function (error) {
                     //Request Failed
-                    mpwchat.log.write("[MPW Chat]{Error} " + error.message);
+                    mpwchat.log.write("[GOGOBUY Chat]{Error} " + error.message);
                     conn.stop();
                     joinedChat = false;
                 });
@@ -81,7 +81,7 @@
     //伺服器訊息: 使用者狀態
     gHub.on('userInfoResults', function (results) {
         if (results) {
-            mpwchat.log.write("[MPW Chat] Joined Chat");
+            mpwchat.log.write("聊天室開啟");
             document.getElementById("btnJoin").value = "Disconnect";
         }
         else {
@@ -92,7 +92,7 @@
 
     //Hub 事件: 斷線事件
     conn.disconnected(function () {
-        mpwchat.log.write("Disconnected");
+        mpwchat.log.write("已離線");
         document.getElementById("txaUsers").value = "";
         document.getElementById("btnJoin").value = "Join";
         joinedChat = false;
@@ -100,30 +100,30 @@
 
     //伺服器訊息: 新使用者加入
     gHub.on('newUser', function (user) {
-        document.getElementById("txaChat").value += "User '" + user + "' Joined the chat\n";
+        document.getElementById("txaChat").value += "會員 '" + user + "' 加入了聊天\n";
     });
 
     //伺服器訊息: 加入聊天的帳號列表
-    gHub.on('userList', function (users, userCount) {
-        try {
-            var userList = document.getElementById("txaUsers");
-            var list = "";
-            for (var i = 0; i < userCount; i++) {
-                list += users[i];
-                list += '\n';
-            }
-            userList.value = list;
-        } catch (ex) {
-            mpwchat.log.write("[MPW Chat]{Error} " + ex.message);
-        }
-    });
+    //gHub.on('userList', function (users, userCount) {
+    //    try {
+    //        var userList = document.getElementById("txaUsers");
+    //        var list = "";
+    //        for (var i = 0; i < userCount; i++) {
+    //            list += users[i];
+    //            list += '\n';
+    //        }
+    //        userList.value = list;
+    //    } catch (ex) {
+    //        mpwchat.log.write("[GOGOBUY Chat]{Error} " + ex.message);
+    //    }
+    //});
 
     //伺服器訊息: 使用者離開
     gHub.on('userLeft', function (user) {
         try {
-            document.getElementById("txaChat").value += "User '" + user + "' Disconnected \n";
+            document.getElementById("txaChat").value += "會員 '" + user + "' 已經離線 \n";
         } catch (ex) {
-            mpwchat.log.write("[MPW Chat]{Error} " + ex.message);
+            mpwchat.log.write("[GOGOBUY Chat]{Error} " + ex.message);
         }
     });
 
@@ -133,7 +133,7 @@
             document.getElementById("txaChat").value += username + ": " + message + '\n';
 
         } catch (ex) {
-            mpwchat.log.write("[MPW Chat]{Error} " + ex.message);
+            mpwchat.log.write("[GOGOBUY Chat]{Error} " + ex.message);
         }
     });
 
