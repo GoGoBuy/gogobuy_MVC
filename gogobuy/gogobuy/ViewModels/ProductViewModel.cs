48f6ba62ba6f13acfc32a69a1b8619d1ff9bcf96
Mars62689 <mars62689@gmail.com>
2021-07-16 01:10:41 +0800

    feat: 新增搜尋結果排序功能、搜尋頁面日期及價錢 格式化 1.ProductViewMode.csl新增格式化字串 2.SellSearchResult.cshtml新增排序js

--- gogobuy/gogobuy/ViewModels/ProductViewModel.cs
+++ gogobuy/gogobuy/ViewModels/ProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,10 +12,12 @@ namespace gogobuy.ViewModels
         public int fProductID { get; set; }
         public string fProductName { get; set; }
         public string fImgPath { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0}")]
         public decimal? fPrice { get; set; }
         public string fProductLocation { get; set; }
         public int? fQuantity { get; set; }
         public string fArrivalTime { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime? fUpdateTime { get; set; }
         public string fDescription { get; set; }
         public bool isLike { get; set; }
